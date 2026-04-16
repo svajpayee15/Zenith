cf0fe86583c609f3f5d0546ec1f14fde2753eec3
Pratham Vajpayee <prathamvajpayee65@gmail.com>
2026-04-17 03:28:35 +0530

    Update register.command.js

--- src/bot/commands/register.command.js
+++ src/bot/commands/register.command.js
@@ -55,7 +55,7 @@ async function register(
       .setLabel("Connect Wallet")
       .setStyle(ButtonStyle.Link)
       .setURL(
-        `http://localhost:3000/auth/approve?userId=${newApprovalInitiated.userId}&nonce=${newApprovalInitiated.nonce}`
+        `https://zenith-e0xa.onrender.com/auth/approve?userId=${newApprovalInitiated.userId}&nonce=${newApprovalInitiated.nonce}`
       )
       .setEmoji("🔗")
   );
